c20cc56b7423139d7b8e3f65c92dbd1af7f85730
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2024-11-30 20:43:49 +0500

    edit

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = async () => {
     return (
         <div className='fixed top-0 left-0 right-0 bg-white/10 backdrop-blur-lg border-b-2 border-sky-400/100 rounded-b-xl shadow-xl rounded-e-lg mx-auto flex justify-between items-center p-3 bg-mirror bg-opacity-30'>
             <div className='text-white'>
-                <h1 className='text-sky-400 font-bold font-sans text-3xl'>My website</h1>
+                <h1 className='text-sky-400 font-bold font-sans text-3xl'>InnoWebSpace</h1>
             </div>
             <div>
                 <div className='bg-sky-400 hover:bg-sky-300 px-2 py-2 rounded-md space-x-4 m-1'><SessionWrapper>
