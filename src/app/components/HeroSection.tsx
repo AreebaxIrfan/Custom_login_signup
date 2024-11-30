31ba036206dec3372b587a02416603ed730a6241
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2024-11-30 19:58:08 +0500

    editted

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -5,8 +5,8 @@ const HeroSection = () => {
         <div className='min-h-screen bg-black'>
             <div>
                 <body className="bg-black flex items-center justify-center min-h-screen" style={{ backgroundImage: 'url(/image.png)', backgroundSize: 'cover' }}>
-                    <div className="bg-white/30 backdrop-blur-lg border border-white/50 rounded-lg shadow-xl p-10 text-center">
-                        <h1 className="text-2xl font-bold text-white mb-4">Hi👋! Welcome to te page</h1>
+                    <div className="bg-white/30 backdrop-blur-lg border border-sky-400/100 rounded-lg shadow-xl p-11 text-center">
+                        <h1 className="text-4xl font-bold text-white mb-4">Hi👋! Welcome to the page</h1>
                         <p className="text-white">Please ! Sign In the page ...</p>
                     </div>
                 </body>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import Navbar from './../components/Navbar';
+import Image from 'next/image';
 
 const page = () => {
     return (
         <div className='min-h-screen' style={{ backgroundImage: 'url(/image.png)', backgroundSize: 'cover' }}>
             <Navbar />
-            <div className='flex items-center justify-center pt-52 '>
-                <div className="bg-white/30 backdrop-blur-lg border border-white/50 rounded-lg shadow-xl  p-10 text-center">
-                    <h1 className="text-2xl font-bold text-white mb-4">Welcome Back to our website</h1>
-                    <p className="text-white">Your account registerd ...</p>
+            <div className=' items-center justify-center pt-48 grid sm:grid-cols-2 gap-11  p-16'>
+                <div className="bg-white/30 backdrop-blur-lg border border-sky-400/100 rounded-lg shadow-xl p-10 text-center">
+                    <h1 className="text-5xl font-serif font-bold text-white mb-4">Welcome to my website!</h1>
+                    <p className=" text-2xl font-semibold text-sky-400"> Where creativity, knowledge, and fun come together ...</p>
+                </div>
+                <div>
+                    <Image src='/dps-1.png'
+                    height={100}
+                    width={450}
+                    alt='image'
+                    className=' rounded-xl'/>
                 </div>
 
                 
